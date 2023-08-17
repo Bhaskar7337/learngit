e569996816256d626479b35a483f5e95bad20925
Bhaskar7337 <141061438+Bhaskar7337@users.noreply.github.com>
2023-08-17 20:08:20 +0530

    implicitytypecasting

---
+++ ExplicitTypeCastingDemo.java
@@ -0,0 +1,11 @@
+class ExplicitTypeCastingDemo
+{
+	public static void main(String[]args)
+	{ 
+		//implicit TypeCasting
+		byte b=10;
+         int  i=b;
+		System.out.println(i);
+		System.out.println(b);
+	}
+}
---
+++ ExplicitTypeCastingDemo1.java
@@ -0,0 +1,10 @@
+class ExplicitTypeCastingDemo1
+{
+	public static void main(String[]args)
+	{ 
+		//ExplicitTypeCastingDemo
+		int i=1000;
+		byte b=(byte)i;
+     	System.out.println(b);
+	}
+}
